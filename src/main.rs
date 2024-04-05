cc6647d40be9359e0c033a0d4ec2f1d08c3a745c
philingood <98781376+philingood@users.noreply.github.com>
2024-04-06 00:18:55 +0300

    add .env file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::io::{BufRead, Write};
+use dotenv::dotenv;
+use std::env;
 
 fn main() {
-    let listner = std::net::TcpListener::bind("127.0.0.1:8080").unwrap();
+    dotenv().ok();
+    let server_adress = env::var("SERVER_ADRESS").unwrap();
+    let listner = std::net::TcpListener::bind(server_adress).unwrap();
     for mut stream in listner.incoming().flatten() {
         let mut rdr = std::io::BufReader::new(&stream);
         let mut line = String::new();
@@ -16,12 +20,9 @@ fn main() {
                 let mut p = std::path::PathBuf::new();
                 p.push("site");
                 p.push(resource.trim_start_matches("/"));
-                if resource.ends_with("/") {
-                    // p.push("site");
-                    p.push("index.html");
-                }
+                if resource.ends_with("/") { p.push("index.html");}
                 stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
-                println!("Path to file: {:?}", p);
+                println!("HTTP/1.1 200 OK");
                 stream.write_all(&std::fs::read(p).unwrap()).unwrap();
             }
             _ => todo!()
